afe7e8b9c1bd1e3ccb05771493de22cd6b0cc5da
Nowimps8 <31818901+Nowimps8@users.noreply.github.com>
2019-09-13 13:11:38 -0600

    Update items.lua

--- server/items.lua
+++ server/items.lua
@@ -5,41 +5,41 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 ESX.RegisterUsableItem('gauze', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
-	TriggerClientEvent('nw_injury:items:gauze', source)
+	TriggerClientEvent('mythic_hospital:items:gauze', source)
 end)
 
 ESX.RegisterUsableItem('bandage', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
-	TriggerClientEvent('nw_injury:items:bandage', source)
+	TriggerClientEvent('mythic_hospital:items:bandage', source)
 end)
 
 ESX.RegisterUsableItem('firstaid', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
-	TriggerClientEvent('nw_injury:items:firstaid', source)
+	TriggerClientEvent('mythic_hospital:items:firstaid', source)
 end)
 
 ESX.RegisterUsableItem('medkit', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
-	TriggerClientEvent('nw_injury:items:medkit', source)
+	TriggerClientEvent('mythic_hospital:items:medkit', source)
 end)
 
 ESX.RegisterUsableItem('vicodin', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
-	TriggerClientEvent('nw_injury:items:vicodin', source)
+	TriggerClientEvent('mythic_hospital:items:vicodin', source)
 end)
 
 ESX.RegisterUsableItem('hydrocodone', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
-	TriggerClientEvent('nw_injury:items:hydrocodone', source)
+	TriggerClientEvent('mythic_hospital:items:hydrocodone', source)
 end)
 
 ESX.RegisterUsableItem('morphine', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
-	TriggerClientEvent('nw_injury:items:morphine', source)
+	TriggerClientEvent('mythic_hospital:items:morphine', source)
 end)
